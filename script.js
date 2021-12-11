41ec41f7788e9f7f6d2cb25ee3826effcb638f6f
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2021-12-10 18:01:57 -0800

    Added another mad lib to js and changed some margins in css as well as opening page name to 'instructions'

--- script.js
+++ script.js
@@ -53,8 +53,8 @@ const libs = [
                     alert("Please enter an answer into every field")
                 }
                 else {
-                    paragraph.innerHTML = `There once was a reindeer named Rudolph who had a big ${r1.value} ${r2.value}. <br>
-                    When Rudolph would go to the ${r3.value} everyone would tease him. <br> This made him very ${r4.value}.`
+                    paragraph.innerHTML = `There once was a reindeer named Rudolph who had a big <em>${r1.value}</em> <em>${r2.value}</em>. <br>
+                    When Rudolph would go to the <em>${r3.value}</em> everyone would tease him. <br> This made him very <em>${r4.value}</em>.`
     
                     buttondiv.removeChild(submitButton)
                 }
@@ -74,7 +74,83 @@ const libs = [
     },
     {
         Title: "An Elf Party",
-        Story: null
+        Story: function loadElf() {
+            clearMain()
+            
+            const mainDiv = document.querySelector('#main')
+            
+            const libdiv = document.createElement('div')
+            libdiv.classList.add('libdiv')
+            mainDiv.appendChild(libdiv)
+        
+            const libtitle = document.createElement('h3')
+            libtitle.classList.add('libtitle') 
+            libtitle.innerHTML = "An Elf Party"
+            libdiv.appendChild(libtitle)
+
+            const instructions = document.createElement('h2')
+            instructions.innerHTML = "Fill out these fields to generate your own silly mad libs story instantly!"
+            instructions.setAttribute('class', 'instructions')
+            libdiv.appendChild(instructions)
+        
+            const input1 = '<input id="r1" class="libinput" type="text" placeholder="First Name (Male)" required>'
+            const input2 = '<input id="r2" class="libinput" type="text" placeholder="Adjective" required>'
+            const input3 = '<input id="r3" class="libinput" type="text" placeholder="Noun (plural)" required>'
+            const input4 = '<input id="r4" class="libinput" type="text" placeholder="Proper Noun (Location)" required>'
+            const input5 = '<input id="r5" class="libinput" type="text" placeholder="Noun (Item singular)" required>'
+            const input6 = '<input id="r6" class="libinput" type="text" placeholder="Noun (Item singular)" required>'
+            const input7 = '<input id="r7" class="libinput" type="text" placeholder="Noun (Item plural)" required>'
+            const input8 = '<input id="r8" class="libinput" type="text" placeholder="Adjective" required>'
+        
+            const paragraph = document.createElement('p')
+            paragraph.classList.add('libparagraph')
+            paragraph.innerHTML = `${input1}<br>${input2}<br>${input3}<br>${input4}<br>${input5}<br>${input6}<br>${input7}<br>${input8}`
+            libdiv.appendChild(paragraph)
+        
+            const buttondiv = document.createElement('div')
+            buttondiv.classList.add('buttondiv')
+            libdiv.appendChild(buttondiv)
+        
+            const submitButton = document.createElement('button')
+            submitButton.classList.add('bottombutton')
+            submitButton.setAttribute('id', 'submit')
+            submitButton.innerHTML = 'Submit'
+            buttondiv.appendChild(submitButton)
+
+            submitButton.addEventListener('click', (e) => {
+                const r1 = document.querySelector('#r1')
+                const r2 = document.querySelector('#r2')
+                const r3 = document.querySelector('#r3')
+                const r4 = document.querySelector('#r4')
+                const r5 = document.querySelector('#r5')
+                const r6 = document.querySelector('#r6')
+                const r7 = document.querySelector('#r7')
+                const r8 = document.querySelector('#r8')
+                
+                if (r1.value == ''|| r2.value == ''|| r3.value == ''|| r4.value == '' || r5.value == ''|| r6.value == ''|| r7.value == ''|| r8.value == '') {
+                    alert("Please enter an answer into every field")
+                }
+                else {
+                    paragraph.innerHTML = `<em>${r1.value}</em> is the head elf in the north pole and was put in charge of this years christmas party.<br>
+                    The theme of the party is <em>${r2.value}</em> <em>${r3.value}</em>.<br>
+                    He needed to buy some supplies so he went to <em>${r4.value}</em> and bought a <em>${r5.value}</em>, a <em>${r6.value}</em>, and <em>${r7.value}</em>.<br>
+                    The party went off without a hitch and everyone said they had a <em>${r8.value}</em> time!`
+    
+                    buttondiv.removeChild(submitButton)
+                }
+
+            })
+        
+            const returnButton = document.createElement('button')
+            returnButton.classList.add('bottombutton')
+            returnButton.setAttribute('id', 'return')
+            returnButton.innerHTML = 'Return'
+            buttondiv.appendChild(returnButton)
+
+            returnButton.addEventListener('click', (e) => {
+                loadHome(libs)
+            })
+        }
     },
     {
         Title: "Santa's Accident",
